b9a8862aaf583125f353617b205fe8e4ba6c2bb5
ming736-2 <80364687+ming736-2@users.noreply.github.com>
2024-09-13 18:44:48 -0400

    host fonts locally, update navbar to be partially dynamic, and a gitignore

--- components/head.js
+++ components/head.js
@@ -1,14 +1,15 @@
 class SiteNavbarTop extends HTMLElement {
-    constructor() {
-      super();
-    };
-    connectedCallback() {
-      function isactive(t,page) {
-        let active = page == t.getAttribute("active") ? "active" : "inactive"
-        //console.log(active, t, t.getAttribute("root")+page)
-        return active
-      }
-        this.innerHTML = `
+  constructor() {
+    super();
+  };
+  connectedCallback() {
+    function isactive(t, page) {
+      let active = page == location.pathname.split("/")[location.pathname.split("/").length-1] ? "active" : "inactive"
+      //console.log(active, t, t.getAttribute("root")+page)
+      return active
+    }
+    this.innerHTML = `
+        <div class="site-banner">i might redesign this site soon</div>
         <script>
         function myFunction() {
           document.getElementById("myDropdown").classList.toggle("show");
@@ -28,24 +29,24 @@ class SiteNavbarTop extends HTMLElement {
         </script>
         <header>
           <div class="topnav">
-            <a class="${isactive(this,"index.html")}" href="${this.getAttribute("root")}index.html">Home</a>
-            <a class="${isactive(this,"mingchat.html")}" href="${this.getAttribute("root")}mingchat.html">MingChat</a>
+            <a class="${isactive(this, "index.html")}" href="${this.getAttribute("root")}index.html">Home</a>
+            <a class="${isactive(this, "mingchat.html")}" href="${this.getAttribute("root")}mingchat.html">MingChat</a>
             <div class="dropdown">
               <button class="dropbtn">Documentation
                 <i class="fa fa-caret-down"></i>
               </button>
               <div class="dropdown-content">
-                <a href="${this.getAttribute("root")}w93/2.x/docs" class="${isactive(this,"w93/2.x/docs")}">Windows 93</a>
-                <a href="${this.getAttribute("root")}w93/trollbox/2.1/docs" class="${isactive(this,"w93/trollbox/2.1/docs")}">Trollbox</a>
+                <a href="${this.getAttribute("root")}w93/2.x/docs" class="${isactive(this, "w93/2.x/docs")}">Windows 93</a>
+                <a href="${this.getAttribute("root")}w93/trollbox/2.1/docs" class="${isactive(this, "w93/trollbox/2.1/docs")}">Trollbox</a>
               </div>
             </div>
-            <!--<a href="${this.getAttribute("root")}socialhub/index.html" class="${isactive(this,"socialhub/index.html")}">Social Hub</a>-->
+            <!--<a href="${this.getAttribute("root")}socialhub/index.html" class="${isactive(this, "socialhub/index.html")}">Social Hub</a>-->
             <!--<a href="SaveDecrypt.html">FS Save File</a>-->
             <!--<a href="#about">About</a>-->
           </div>
         </header>
         `;
-      }
+  }
 }
 /**
  * 
@@ -53,7 +54,7 @@ class SiteNavbarTop extends HTMLElement {
  */
 
 function download(downloaddata) {
-  const dataArr = downloaddata.split(";",2)
+  const dataArr = downloaddata.split(";", 2)
   const downloadType = dataArr[0]
   const downloadLink = dataArr[1]
   if (downloadType == "external") {
@@ -69,7 +70,7 @@ class SiteDownload extends HTMLElement {
     console.log(reltype)
     this.innerHTML = `
     <div class="download-frame">
-            <span style="color:${reltype == "latest" ? "lime" : reltype == "yellow" ? "blue" : reltype == "beta" ? "blue" : "red" }">${reltype.toUpperCase()}</span>
+            <span style="color:${reltype == "latest" ? "lime" : reltype == "yellow" ? "blue" : reltype == "beta" ? "blue" : "red"}">${reltype.toUpperCase()}</span>
             <strong class="download-frame-name">${this.getAttribute("name")}</strong>
             <p class="download-frame-desc">${this.getAttribute("desc")}</p>
             <button class="downloadbtn" type="button" value="${this.getAttribute("ref")}" title="Download"  onclick="download(this.value)" >Download</button>
