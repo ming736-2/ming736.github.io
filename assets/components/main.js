6ac954b5c1b5dad13444773133d14ac7bb37e05c
ming736-2 <80364687+ming736-2@users.noreply.github.com>
2024-09-13 20:34:33 -0400

    add embed

--- assets/components/main.js
+++ assets/components/main.js
@@ -14,13 +14,4 @@ script.onload = function () {
     const navbarTop = document.createElement('navbar-top');
     document.body.prepend(navbarTop);
 };
-document.head.insertAdjacentHTML("afterbegin",`<meta property="og:title" content="ming736's site" />
-        <meta property="og:type" content="website" />
-        <meta property="og:url" content="https://ming736.github.io${location.pathname}" />
-        <meta property="og:description" content="welcome to my site!" />
-        <meta property="og:site_name" content="ming736's site" />
-        <meta name="twitter:card" content="summary_large_image" />
-        <meta name="twitter:title" content="ming736's site" />
-        <meta name="twitter:description" content="welcome to my site!" />
-        <meta name="twitter:site" content="@ming736_" />`)
 document.head.appendChild(script);
